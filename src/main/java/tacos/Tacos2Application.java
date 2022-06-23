1709a91926598299eef46fd189a322b1dec5693f
furkancavusoglu <cfurkannn@outlook.com>
2022-06-23 14:35:13 +0300

    Initial commit

---
+++ src/main/java/tacos/Tacos2Application.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Tacos2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tacos2Application.class, args);
+    }
+
+}
